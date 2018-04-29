ad68aae65d50cd825bcec05dccede44a6a38a9d5
tikubonn <https://twitter.com/tikubonn>
2018-04-29 14:46:35 +0900

    a print out function of print_unicos_as_utf32 and print_unicos_as_utf32_ln was added.

---
+++ debug/src/print_unicos_as_utf32.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <unico.h>
+#include <stdio.h>
+
+extern void print_unicos_as_utf32be (unicos*, FILE*);
+extern void print_unicos_as_utf32le (unicos*, FILE*);
+extern void print_unicos_as_utf32be_ln (unicos*, FILE*);
+extern void print_unicos_as_utf32le_ln (unicos*, FILE*);
