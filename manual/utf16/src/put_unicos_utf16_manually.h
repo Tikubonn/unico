f021b3ef32845c36c560d82fd0cb327c5fe0f7e6
tikubonn <https://twitter.com/tikubonn>
2018-04-29 20:24:35 +0900

    removed unnecessary comments and include declare.

--- manual/utf16/src/put_unicos_utf16_manually.h
+++ manual/utf16/src/put_unicos_utf16_manually.h
@@ -2,5 +2,3 @@
 #include <unico.h>
 
 extern int put_unicos_utf16_manually (unsigned char, unicos_utf16*);
-// extern int put_unicos_utf16be_manually (unsigned char, unicos_utf16*);
-// extern int put_unicos_utf16le_manually (unsigned char, unicos_utf16*);
--- manual/utf32/src/put_unicos_utf32_manually.h
+++ manual/utf32/src/put_unicos_utf32_manually.h
@@ -2,5 +2,3 @@
 #include <unico.h>
 
 extern int put_unicos_utf32_manually (unsigned char, unicos_utf32*);
-// extern int put_unicos_utf32be_manually (unsigned char, unicos_utf32*);
-// extern int put_unicos_utf32le_manually (unsigned char, unicos_utf32*);
