e7fd6634aa30d00c01c7eb8fdbf4c2ec4b36e47b
tikubonn <https://twitter.com/tikubonn>
2018-04-29 20:05:28 +0900

    redefined some functions for exception safety.

--- manual/utf16/src/put_unicos_utf16_manually.c
+++ manual/utf16/src/put_unicos_utf16_manually.c
@@ -1,6 +1,8 @@
 #include <unico.h>
 #include "unicos_utf16_utilities.h"
 
+/* bom */
+
 static int put_unicos_utf16bom_manually (unsigned char character, unicos_utf16 *uniout){
 	switch (uniout->count){
 		case 0:
@@ -38,119 +40,164 @@ static int put_unicos_utf16bom_manually (unsigned char character, unicos_utf16 *
 			uniout->count = 0;
 			return 0;
 		default: 
-			return 1;
+			uniout->value1 = 0;
+			uniout->value2 = 0;
+			uniout->count = 0;
+			return UNICOS_UTF16_SYNTAX_ERROR;
+	}
+}
+
+/* big endian */
+
+static int put_unicos_utf16be_manually_in0 (unsigned char character, unicos_utf16 *uniout){
+	uniout->value1 = character;
+	uniout->count++;
+	return 0;
+}
+
+static inline int put_unicos_utf16be_manually_in1 (unsigned char character, unicos_utf16 *uniout){
+	unico code = uniout->value1;
+	code <<= 8;
+	code |= character;
+	if (is_bom_big_endian_utf16(code)){
+		uniout->value1 = 0;
+		uniout->count = 0;
+		return UNICOS_UTF16_SYNTAX_ERROR;
+	}
+	if (is_bom_little_endian_utf16(code)){
+		uniout->value1 = 0;
+		uniout->count = 0;
+		return UNICOS_UTF16_SYNTAX_ERROR;
+	}
+	if (is_high_surrogate_utf16(code)){
+		uniout->value1 = code;
+		uniout->count++;
+		return 0;
+	}
+	else {
+		int status = put_unicos_manually(code, (unicos*)uniout);
+		if (status == UNICOS_NOT_ENOUGH_MEMORY)
+			return UNICOS_NOT_ENOUGH_MEMORY;
+		uniout->value1 = 0;
+		uniout->count = 0;
+		return 0;
+	}
+}
+
+static int put_unicos_utf16be_manually_in2 (unsigned char character, unicos_utf16 *uniout){
+	uniout->value2 = character;
+	uniout->count++;
+	return 0;
+}
+
+static int put_unicos_utf16be_manually_in3 (unsigned char character, unicos_utf16 *uniout){
+	unico code = uniout->value2;
+	code <<= 8;
+	code |= character;
+	if (is_low_surrogate_utf16(code)){
+		unico decoded = decode_surrogate_utf16(uniout->value1, code);
+		int status = put_unicos_manually(decoded, (unicos*)uniout);
+		if (status == UNICOS_NOT_ENOUGH_MEMORY)
+			return UNICOS_NOT_ENOUGH_MEMORY;
+		else {
+			uniout->value1 = 0;
+			uniout->value2 = 0;
+			uniout->count = 0;
+			return 0;
+		}
+	}
+	else {
+		uniout->value1 = 0;
+		uniout->value2 = 0;
+		uniout->count = 0;
+		return UNICOS_UTF16_SYNTAX_ERROR;	
 	}
 }
 
 static int put_unicos_utf16be_manually (unsigned char character, unicos_utf16 *uniout){
 	switch (uniout->count){
-		case 0:
-			uniout->value1 <<= 8;
-			uniout->value1 |= character;
-			uniout->count++;
-			return 0;
-		case 1:
-			uniout->value1 <<= 8;
-			uniout->value1 |= character;
-			uniout->count++;
-			/* found bom */
-			if (is_bom_big_endian_utf16(uniout->value1)){
-				uniout->value1 = 0;
-				uniout->count = 0;
-				return 1;
-			}
-			/* found bom */
-			if (is_bom_little_endian_utf16(uniout->value1)){
-				uniout->value1 = 0;
-				uniout->count = 0;
-				return 1;
-			}
-			/* found high surrogate */
-			if (!is_high_surrogate_utf16(uniout->value1)){
-				put_unicos_manually(uniout->value1, (unicos*)uniout);
-				uniout->value1 = 0;
-				uniout->count = 0;
-				return 0;
-			}
-			return 0;
-		case 2:
-			uniout->value2 <<= 8;
-			uniout->value2 |= character;
-			uniout->count++;
-			return 0;
-		case 3:
-			uniout->value2 <<= 8;
-			uniout->value2 |= character;
-			uniout->count++;
-			if (is_low_surrogate_utf16(uniout->value1)){
-				unico uni = decode_surrogate_utf16(uniout->value1, uniout->value2);
-				put_unicos_manually(uni, (unicos*)uniout);
-				uniout->value1 = 0;
-				uniout->value2 = 0;
-				uniout->count = 0;
-				return 0;
-			}
-			uniout->value1 = 0;
-			uniout->value2 = 0;
-			uniout->count = 0;
-			return 1;
-		default:
-			return 1;
+		case 0: return put_unicos_utf16be_manually_in0(character, uniout);
+		case 1: return put_unicos_utf16be_manually_in1(character, uniout);
+		case 2: return put_unicos_utf16be_manually_in2(character, uniout);
+		case 3: return put_unicos_utf16be_manually_in3(character, uniout);
+		default: return UNICOS_UTF16_SYNTAX_ERROR;
+	}
+}
+
+/* little endian */
+
+static int put_unicos_utf16le_manually_in0 (unsigned char character, unicos_utf16 *uniout){
+	uniout->value1 = character;
+	uniout->count++;
+	return 0;
+}
+
+static int put_unicos_utf16le_manually_in1 (unsigned char character, unicos_utf16 *uniout){
+	unico code = uniout->value1;
+	code |= character << 8;
+	if (is_bom_big_endian_utf16(code)){
+		uniout->value1 = 0;
+		uniout->count = 0;
+		return UNICOS_UTF16_SYNTAX_ERROR;
+	}
+	if (is_bom_little_endian_utf16(code)){
+		uniout->value1 = 0;
+		uniout->count = 0;
+		return UNICOS_UTF16_SYNTAX_ERROR;
+	}
+	if (is_high_surrogate_utf16(code)){
+		uniout->value1 = code;
+		uniout->count++;
+		return 0;
+	}
+	else {
+		int status = put_unicos_manually(code, (unicos*)uniout);
+		if (status == UNICOS_NOT_ENOUGH_MEMORY)
+			return UNICOS_NOT_ENOUGH_MEMORY;
+		uniout->value1 = 0;
+		uniout->count = 0;
+		return 0;
+	}
+}
+
+static int put_unicos_utf16le_manually_in2 (unsigned char character, unicos_utf16 *uniout){
+	uniout->value2 = character;
+	uniout->count++;
+	return 0;
+}
+
+static int put_unicos_utf16le_manually_in3 (unsigned char character, unicos_utf16 *uniout){
+	unico code = uniout->value1;
+	code |= character << 8;
+	if (is_low_surrogate_utf16(code)){
+		unico decoded = decode_surrogate_utf16(uniout->value1, code);
+		int status = put_unicos_manually(decoded, (unicos*)uniout);
+		if (status == UNICOS_NOT_ENOUGH_MEMORY)
+			return UNICOS_NOT_ENOUGH_MEMORY;
+		uniout->value1 = 0;
+		uniout->value2 = 0;
+		uniout->count = 0;
+		return 0;
+	}
+	else {
+		uniout->value1 = 0;
+		uniout->value2 = 0;
+		uniout->count = 0;
+		return UNICOS_UTF16_SYNTAX_ERROR;
 	}
 }
 
 static int put_unicos_utf16le_manually (unsigned char character, unicos_utf16 *uniout){
 	switch (uniout->count){
-		case 0:
-			uniout->value1 = character;
-			uniout->count++;
-			return 0;
-		case 1:
-			uniout->value1 |= character << 8;
-			uniout->count++;
-			/* found bom */
-			if (is_bom_big_endian_utf16(uniout->value1)){
-				uniout->value1 = 0;
-				uniout->count = 0;
-				return 1;
-			}
-			/* found bom */
-			if (is_bom_little_endian_utf16(uniout->value1)){
-				uniout->value1 = 0;
-				uniout->count = 0;
-				return 1;
-			}
-			/* found high surrogate */
-			if (!is_high_surrogate_utf16(uniout->value1)){
-				put_unicos_manually(uniout->value1, (unicos*)uniout);
-				uniout->value1 = 0;
-				uniout->count = 0;
-				return 0;
-			}
-			return 1;
-		case 2:
-			uniout->value2 = character;
-			uniout->count++;
-			return 0;
-		case 3:
-			uniout->value2 |= character << 8;
-			uniout->count++;
-			if (is_low_surrogate_utf16(uniout->value1)){
-				unico uni = decode_surrogate_utf16(uniout->value1, uniout->value2);
-				put_unicos_manually(uni, (unicos*)uniout);
-				uniout->value1 = 0;
-				uniout->value2 = 0;
-				uniout->count = 0;
-				return 0;
-			}
-			uniout->value1 = 0;
-			uniout->value2 = 0;
-			uniout->count = 0;
-			return 1;
-		default:
-			return 1;
+		case 0: return put_unicos_utf16le_manually_in0(character, uniout);
+		case 1: return put_unicos_utf16le_manually_in1(character, uniout);
+		case 2: return put_unicos_utf16le_manually_in2(character, uniout);
+		case 3: return put_unicos_utf16le_manually_in3(character, uniout);
+		default: return UNICOS_UTF16_SYNTAX_ERROR;
 	}
 }
+
+/* export routine */
 
 int put_unicos_utf16_manually (unsigned char character, unicos_utf16 *uniout){
 	switch (uniout->endian){
