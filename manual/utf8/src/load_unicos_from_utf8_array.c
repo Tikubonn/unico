73094552b39ea64897b7ef7d2db8412435722f09
tikubonn <https://twitter.com/tikubonn>
2018-05-13 23:36:12 +0900

    fixed source code.

--- manual/utf8/src/load_unicos_from_utf8_array.c
+++ manual/utf8/src/load_unicos_from_utf8_array.c
@@ -5,7 +5,7 @@ extern int load_unicos_from_utf8_array (unsigned char *sequence, size_t size, un
 	make_tmp_unicop_utf8(decoder, uniout);
 	size_t index;
 	for (index = 0; index < size; index++){
-		int status = put_unicop_utf8_manually(sequence[index], uniout);
+		int status = put_unicop_utf8_manually(sequence[index], decoder);
 		if (status) return status;
 	}
 	return 0;
